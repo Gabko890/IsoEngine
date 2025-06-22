72b19da17391ce03e85fd0493a6058c15ed154b1
WonTeu <gabriel.szabo2006@gmail.com>
2025-06-22 20:35:33 +0200

    shaders updated for advanced lighting

--- Renderer.cpp
+++ Renderer.cpp
@@ -98,16 +98,28 @@ const char* Renderer::GetFragmentShaderSource() {
 
         uniform sampler2D baseColorTexture;
         uniform vec3 lightPos;
+        uniform vec3 viewPos;
         uniform vec3 lightColor;
 
         void main() {
             vec3 norm = normalize(Normal);
             vec3 lightDir = normalize(lightPos - FragPos);
-            float diff = max(dot(norm, lightDir), 0.0);
-            vec3 light = diff * lightColor;
+            vec3 viewDir = normalize(viewPos - FragPos);
+            vec3 reflectDir = reflect(-lightDir, norm);
 
+            float ambientStrength = 0.1;
+            vec3 ambient = ambientStrength * lightColor;
+
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor;
+
+            float specularStrength = 0.5;
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+            vec3 specular = specularStrength * spec * lightColor;
+
+            vec3 lighting = ambient + diffuse + specular;
             vec4 texColor = texture(baseColorTexture, TexCoord);
-            FragColor = vec4(texColor.rgb * light, texColor.a);
+            FragColor = vec4(texColor.rgb * lighting, texColor.a);
         }
     )";
 }
